88ab1d9e7c34382813afc2c1eb907033d06c59e4
yagoPRO0404 <yagoa1994@gmail.com>
2025-04-10 00:23:11 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,15 @@
+let allisson_nota_av1 = 8;
+let allisson_nota_av2 = 9.6;
+let allisson_media = (allisson_nota_av1  + allisson_nota_av2) / 2
+
+console.log("Media Allisson:  "  + allisson_media);
+
+console.log("Exemplo com arrays")
+let notas_allisson = [8, 9.6, ];
+console.log(notas_allisson);
+
+console.log("AV1: "+notas_allisson[0]);
+console.log("AV2: "+notas_allisson[0]);
+notas_allisson[2] = (notas_allisson[0]+ notas_allisson[1]) / 2;
+console.log("Media: "+notas_allisson[2]);
+console.log("Todas: "+notas_allisson);
